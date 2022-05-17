26ee5ba16b58a77c13389ce3084df134a6769914
dinhtuannam <namdinhtuan5@gmail.com>
2022-05-17 23:23:55 +0700

    pull

--- ad/index.php
+++ ad/index.php
@@ -153,6 +153,8 @@
          include_once 'pages/ui-features/quyền/qlquyen.php';
       else if($_GET['act'] =="qltk")
         include_once 'pages/ui-features/tài khoản/taikhoan.php';
+        else if($_GET['act'] =="tkkd")
+        include_once 'pages/ui-features/Thống kê/thongkekd.php';
       else if($_GET['act'] =="thongke")
         include_once 'pages/ui-features/Thống kê/thongkesp.php';
 
